ee8b2de645b33f2e4eb6fe066ba48c8610744cb6
Pypsik0904 <mashkaminyazevaqq@gmail.com>
2025-05-28 17:32:44 +0300

    app.py

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template
 
-app = Flask("", template_folder="", static_folder="")
+app = Flask("PlushShop", template_folder="", static_folder="")
 
 @app.route('/') # декоратор, приписывающий к пути / функцию index
 def index():
     return render_template("index.html")
+
+
+@app.route('/cart')
+def cart():
+    return render_template("cart.html")
+
+@app.route('/help')
+def help():
+    return render_template("help.html")
+
 
 app.run()
